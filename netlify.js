c333451ebbb200775b72f90b42863c7083747745
Yeltsin-Z <yeltsin@drivetrain.ai>
2025-04-10 07:58:44 +0530

    Add Netlify configuration and favicon

---
+++ netlify.js
@@ -0,0 +1,3 @@
+// This is a placeholder file to indicate this is a static site
+// No JavaScript functionality needed at the build level
+console.log('Elite Group Facility Management Service - Static Site'); 
